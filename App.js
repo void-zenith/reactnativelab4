34e03d09959878134b70bc3963d4de798747822f
void-zenith <zenithrajbhandari@gmail.com>
2023-09-29 09:00:51 -0400

    map

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.mapView}
+        initialRegion={{
+          latitude: 43.7852,
+          longitude: 79.2282,
+        }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+      ></MapView>
     </View>
   );
 }
@@ -13,8 +20,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapView: {
+    height: "100%",
+    width: "100%",
   },
 });
